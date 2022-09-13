80ee13d7b8e061d0514c825a2453eb4b74432b84
momoc <50790577+momok1@users.noreply.github.com>
2022-09-13 10:18:28 +0800

    reset PC

--- js/home.js
+++ js/home.js
@@ -3,3 +3,12 @@ document.write("learning the js in very beginning");
 function oneFunction(){
     alert("hhhhhhhhhh");
 }
+
+function onbeforeload(event){
+    var e =event || window.event;
+    e.returnValue ="welcome back again!";
+}
+
+window.onload = function () {
+    alert("欢迎光临萌萌小店！");
+}
